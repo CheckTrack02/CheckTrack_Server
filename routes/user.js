f9fbab838936f3a54a25d9a64ade2e03c34ab770
retro3014 <retro3014@naver.com>
2023-07-14 16:59:02 +0000

    add user/get-group-user-entity

--- routes/user.js
+++ routes/user.js
@@ -33,6 +33,19 @@ router
             res.status(200).json(rows);
         });
     })
+    .get("/get-group-user-entity", (req, res) => {
+        console.log("GET GROUP USER ENTITY");
+        const userNo = req.query.userNo;
+        const groupNo = req.query.groupNo;
+        connection.query('SELECT * FROM groupUserTable WHERE userNo = ? AND groupNo = ?', [userNo, groupNo], function (error, rows){
+            if(error) throw error;
+            if(rows.length == 1){
+                res.status(200).json(rows[0]);
+            }else{
+                res.status(401);
+            }
+        });
+    })
 
 
 module.exports = router;
